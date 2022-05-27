63f09ea68d1b0f5bc33c674e8c4af3a84fb94328
EnderC00kiez <49785023+EnderC00kiez@users.noreply.github.com>
2022-05-27 14:30:38 +0000

    ae

--- setup.sh
+++ setup.sh
@@ -46,4 +46,4 @@ if ! [ -x "$(command -v ruby)" ]; then
         exit 1
     fi
 fi
-curl -O https://static.enderc00kiez.xyz/amethyst/setup.py
+wget https://static.enderc00kiez.xyz/amethyst/setup.py
